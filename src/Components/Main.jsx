9e0bf66320b853d6e9247bbb2ff27726e49829f0
Timothy Young <bht4069@autuni.ac.nz>
2023-05-03 23:30:08 +1200

    created some containers and an onClick function

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,14 +1,40 @@
 import { login } from "./Database.jsx";
 import Header from "./Header.jsx";
 import { auth } from "../App.js";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import "./Main.css";
 
 function Main() {
   return (
     <div className="login">
       <Header user={auth.currentUser} />
       <p>Logged in!</p>
+      <div className="stats-container"></div>
+      <div className="buttons-container">
+        <Button variant="contained" onClick={addCollection}>
+          Add Collection
+        </Button>
+        <TextField
+          className="hidden"
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+        />
+        <Button className="hidden" variant="contained">
+          Add Collection
+        </Button>
+      </div>
     </div>
   );
+}
+
+function addCollection() {
+  const hidden = document.getElementsByClassName("hidden");
+  Array.from(hidden).forEach((el) => {
+    el.classList.remove("hidden");
+    el.classList.add("puff-in-center");
+  });
 }
 
 export default Main;
