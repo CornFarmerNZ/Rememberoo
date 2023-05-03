dded5244bcc973f7e6a6f0aa5a3c6781918aa792
Timothy Young <bht4069@autuni.ac.nz>
2023-05-04 00:53:18 +1200

    added stats and more collections to test y-overflow behaviour. added add-collection button

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,6 +4,7 @@ import { auth } from "../App.js";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Collection from "./Collection.jsx";
+import LibraryAddIcon from "@mui/icons-material/LibraryAdd";
 import "./Main.css";
 
 function Main() {
@@ -12,18 +13,44 @@ function Main() {
       <div className="header-container">
         <Header user={auth.currentUser} />
       </div>
-      <div className="stats-container"></div>
+      <div className="stats-container">
+        <div>
+          <div className="font-header">Streak (Days)</div>
+          <div>5</div>
+        </div>
+        <div>
+          <div className="font-header">Collection count</div>
+          <div>23</div>
+        </div>
+        <div>
+          <div className="font-header">Cards remembered</div>
+          <div>1074</div>
+        </div>
+      </div>
       <div className="collection-container">
+        <Collection />
+        <Collection />
+        <Collection />
+        <Collection />
+        <Collection />
+        <Collection />
+        <Collection />
+        <Collection />
+        <Collection />
+        <Collection />
+        <Collection />
         <Collection />
       </div>
       <div className="buttons-container">
-        <Button variant="contained" onClick={addCollection}>
-          Add Collection
-        </Button>
+        <div className="add-new-icon-container font-header">
+          <LibraryAddIcon className="icon-add" onClick={addCollection} />
+          Add a new Collection
+        </div>
+
         <TextField
           className="hidden"
           id="outlined-basic"
-          label="Outlined"
+          label="Collection"
           variant="outlined"
         />
         <Button className="hidden" variant="contained">
@@ -38,7 +65,7 @@ function addCollection() {
   const hidden = document.getElementsByClassName("hidden");
   Array.from(hidden).forEach((el) => {
     el.classList.remove("hidden");
-    el.classList.add("puff-in-center");
+    el.classList.add("slide-in-left");
   });
 }
 
