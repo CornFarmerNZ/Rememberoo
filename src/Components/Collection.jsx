27b6097a01fa2b9075e68e77d2b3473123e6c52f
Timothy Young <bht4069@autuni.ac.nz>
2023-05-04 00:52:31 +1200

    created collection test component

---
+++ src/Components/Collection.jsx
@@ -0,0 +1,14 @@
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
+
+function Collection(props) {
+  return (
+    <div className="collection">
+      <AutoAwesomeIcon fontSize="inherit" className="icon" />
+      <div className="container-right">
+        <div>Ελληνικα</div>
+      </div>
+    </div>
+  );
+}
+
+export default Collection;
